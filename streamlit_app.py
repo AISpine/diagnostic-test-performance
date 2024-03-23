8ff252ca4919adcf49d174023d07793f6892c07b
AISpine <66691551+AISpine@users.noreply.github.com>
2024-03-23 19:40:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ import pandas as pd
 # Function to display the confusion matrix with annotations for multiple specificities
 def display_confusion_matrix(sensitivity, specificities, basket_prevalence, total_population):
     for spec in specificities:
-        disease_cases = total_population * (basket_prevalence/100)
+        disease_cases = total_population * (basket_prevalence)
         tp = (sensitivity/100) * disease_cases
         fn = disease_cases - tp
         tn = (spec/100) * (total_population - disease_cases)
