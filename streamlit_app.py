4406136c9080d9dd35eda9bec1b536c20b2a40d6
AISpine <66691551+AISpine@users.noreply.github.com>
2024-03-23 21:20:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ total_population = 100000  # Example population size for testing
 # Sensitivity and specificities should come from user input or predefined values
 
 # Now call the function to display the confusion matrices
-display_confusion_matrix(sensitivity, specificities, basket_prevalence/100, total_population)
+display_confusion_matrix(sensitivity, specificities, basket_prevalence, total_population)
 
 # Custom title with HTML and Markdown for Tier 2
 st.markdown("<h1 style='text-align: center;'>Confusion Matrix for Tier 2 (Reflex testing)</h1>", unsafe_allow_html=True)
