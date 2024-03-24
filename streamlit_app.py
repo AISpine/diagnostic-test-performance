6395474ce0856f10abeacfa9e7f2243386e4967e
AISpine <66691551+AISpine@users.noreply.github.com>
2024-03-23 20:39:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,9 +99,9 @@ st.markdown("<h1 style='text-align: center;'>Confusion Matrix for Tier 1</h1>", 
 def display_confusion_matrix(sensitivity, specificities, basket_prevalence, total_population):
     for spec in specificities:
         disease_cases = total_population * (basket_prevalence/100)
-        tp = (sensitivity/100) * disease_cases
+        tp = (sensitivity) * disease_cases
         fn = disease_cases - tp
-        tn = (spec/100) * (total_population - disease_cases)
+        tn = (spec) * (total_population - disease_cases)
         fp = (total_population - disease_cases) - tn
         ppv = tp / (tp + fp) if tp + fp > 0 else 0
         npv = tn / (tn + fn) if tn + fn > 0 else 0
