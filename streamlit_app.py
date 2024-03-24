716adbf1c52dc290a98084fca64fa81cd3300e1a
AISpine <66691551+AISpine@users.noreply.github.com>
2024-03-23 20:40:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ def display_confusion_matrix(sensitivity, specificities, basket_prevalence, tota
         disease_cases = total_population * (basket_prevalence/100)
         tp = (sensitivity) * disease_cases
         fn = disease_cases - tp
-        tn = (spec) * (total_population - disease_cases)
+        tn = (spec/100) * (total_population - disease_cases)
         fp = (total_population - disease_cases) - tn
         ppv = tp / (tp + fp) if tp + fp > 0 else 0
         npv = tn / (tn + fn) if tn + fn > 0 else 0
